75e8a672f6298b6ea7758d21f256108dfcc32c52
zulqarnain004 <zulqarnainnaveed4@gmail.com>
2024-04-02 14:42:52 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
